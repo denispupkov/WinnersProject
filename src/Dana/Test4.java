2e0560154230ba2ab72da2d1be1661ea49603abe
usdanakl <usdanakl@gmail.com>
2022-12-23 08:19:07 -0600

    Another very useful method created

--- src/Dana/Test4.java
+++ src/Dana/Test4.java
@@ -4,4 +4,8 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("Test4");
     }
+
+    void method (){
+        System.out.println("Another very useful method");
+    }
 }
