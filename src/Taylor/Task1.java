82f2b83a36e0f7dac149f839f63c8f276b7115c1
taylo <tayloralexisandres@gmail.com>
2022-12-27 19:22:39 -0700

    Pizza

--- src/Taylor/Task1.java
+++ src/Taylor/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         System.out.println(" I love pizza");
         System.out.println(" But I dont like pizza task");
+
     }
 }
