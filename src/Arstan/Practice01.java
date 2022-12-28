4a46fc9de5fbe2ad6b53962d6c04872ba018f88b
arstan <arstankmn@gmail.com>
2022-12-27 19:43:27 -0600

    I added Hello Team

---
+++ src/Arstan/Practice01.java
@@ -0,0 +1,11 @@
+package Arstan;
+
+public class Practice01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Team");
+
+        System.out.println("Hello 01");
+    }
+}
