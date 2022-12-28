4c4f6752c3358f55977800d290e6d2d3120ff4f7
taylo <tayloralexisandres@gmail.com>
2022-12-27 18:57:23 -0700

    Pizza

---
+++ src/Taylor/Task1.java
@@ -0,0 +1,8 @@
+package Taylor;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(" I love pizza");
+        System.out.println(" But I dont like pizza task");
+    }
+}
