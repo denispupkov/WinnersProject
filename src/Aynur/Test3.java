64d54fba8fa7d2cdc894fc212ddd7b05e9693285
ASG <gulisikaynur@gmail.com>
2022-12-27 18:20:00 -0600

    Done

--- src/Aynur/Test3.java
+++ src/Aynur/Test3.java
@@ -4,4 +4,7 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("Test3");
     }
+    void program(){
+        System.out.println("Great program for sharing projects");
+    }
 }
