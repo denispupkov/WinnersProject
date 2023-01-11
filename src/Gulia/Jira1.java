9e812457ed06906281d35c44ff57d9685d9f634b
Gu1ia <burangulovag2@gmail.com>
2023-01-10 20:10:21 -0500

    Updated Jira1, Jira2

--- src/Gulia/Jira1.java
+++ src/Gulia/Jira1.java
@@ -2,6 +2,7 @@ package Gulia;
 
 public class Jira1 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        System.out.println("ANDREIIII!!! WHYYYYYYY");
     }
 }
